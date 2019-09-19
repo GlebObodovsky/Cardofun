4325b7880faf9c90ef836f6276d9cb8f65eb07ec
GlebObodovsky <ya.dmitrievgleb@gmail.com>
2019-09-19 23:33:11 +0300

    * txt files for cities bulk import were compressed

--- Cardofun.DataContext/Seeding/Seed.cs
+++ Cardofun.DataContext/Seeding/Seed.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Threading.Tasks;
 using Cardofun.DataContext.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,21 +16,30 @@ namespace Cardofun.DataContext.Seeding
             if(context.Cities.Any())
                 return;
 
-            var sqlFiles = Directory.GetFiles("../Cardofun.DataContext/Seeding/Resources", "*.sql")
+            var rootPath = "../Cardofun.DataContext/Seeding/Resources";
+
+            var sqlFiles = Directory.GetFiles(rootPath, "*.sql")
                 .OrderBy(fileName => fileName);
 
             context.Database.SetCommandTimeout(TimeSpan.FromMinutes(20));
+            
+            var zipArchives = Directory.GetFiles(rootPath, "*.zip");
+
+            Parallel.ForEach(zipArchives, z => ZipFile.ExtractToDirectory(z, rootPath));
 
             foreach(var fileName in sqlFiles)
             {
                 var sqlQuery = File.ReadAllText(fileName);
                 if(sqlQuery.StartsWith("EXEC"))
-                    context.Database.ExecuteSqlCommand(sqlQuery, new SqlParameter("@FolderPath", Path.GetFullPath("../Cardofun.DataContext/Seeding/Resources")));
+                    context.Database.ExecuteSqlCommand(sqlQuery, new SqlParameter("@FolderPath", Path.GetFullPath(rootPath)));
                 else
                    context.Database.ExecuteSqlCommand(sqlQuery);
             }
 
             context.SaveChanges();
+
+            var txtFiles = Directory.GetFiles(rootPath, "*.txt");
+            Parallel.ForEach(txtFiles, t => File.Delete(t));
         }
     }
 }
